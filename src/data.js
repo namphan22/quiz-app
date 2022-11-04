6e79da7635f9a1592fd8bc0b7a7b12c6083e1ea4
namphan22 <phanvanhoainam22@gmail.com>
2022-11-05 00:40:03 +0700

    init code

---
+++ src/data.js
@@ -0,0 +1,115 @@
+export const question = [
+    {
+        title:'Loài chim nào được yêu thích nhất?',
+        answer:{
+            a:'Chim ưng',
+            b:'Chim cú',
+            c:'Chim đại bàng',
+            d:'Chim sẻ'
+        },
+        correctAnswer:'a'    
+    },
+    {
+        title:'Tên trộm vào nhà thấy em bé đáng yêu thì khen gì?',
+        answer:{
+            a:'Trộm vía',
+            b:'Khen thưởng',
+            c:'Bắt đi',
+            d:'Xinh xỉu'
+        },
+        correctAnswer:'a'    
+    },
+    {
+        title:'Khi mất mạng ta thường thấy con gì?',
+        answer:{
+            a:'Không thấy gì nữa',
+            b:'Thấy buồn',
+            c:'Con khủng long',
+            d:'Thấy màu đen'
+        },
+        correctAnswer:'c'    
+    },
+    {
+        title:'Vì sao Captain America không bao giờ gục ngã?',
+        answer:{
+            a:'Vì anh có sức mạnh',
+            b:'Vì anh cứng cáp',
+            c:'Vì anh có ý chí',
+            d:'All'
+        },
+        correctAnswer:'b'    
+    },
+    {
+        title:'Quần rộng nhất là quần gì?',
+        answer:{
+            a:'Quần đùi',
+            b:'Quần đảo',
+            c:'Quần thụng',
+            d:'Quần sooc'
+        },
+        correctAnswer:'b'    
+    },
+    {
+        title:'Cái gì mà đi thì nằm, đứng cũng nằm, nhưng nằm lại đứng?',
+        answer:{
+            a:'Cái lưng',
+            b:'Cái cổ',
+            c:'Bàn tay',
+            d:'Bàn chân'
+        },
+        correctAnswer:'d'    
+    },
+    {
+        title:'Núi nào mà bị chặt ra từng khúc?',
+        answer:{
+            a:'Thái Sơn',
+            b:'Ngũ Hành Sơn',
+            c:'Himalaya',
+            d:'Hoàng Liên Sơn'
+        },
+        correctAnswer:'a'    
+    },
+    {
+        title:'Cái gì bằng cái vung, vùng xuống ao, đào chẳng thấy, lấy chẳng được?',
+        answer:{
+            a:'Cây cầu',
+            b:'Cái vung',
+            c:'Bóng mặt trăng',
+            d:'Cái thuyền'
+        },
+        correctAnswer:'c'    
+    },
+    {
+        title:'Con gì đập thì sống, không đập thì chết?',
+        answer:{
+            a:'Con Sông',
+            b:'Con ếch',
+            c:'Con đập',
+            d:'Con tim'
+        },
+        correctAnswer:'d'    
+    },
+    {
+        title:'Bỏ ngoài nướng trong, ăn ngoài bỏ trong là gì?',
+        answer:{
+            a:'Xúc xích',
+            b:'Bắp ngô',
+            c:'Cây mía',
+            d:'Củ khoai'
+        },
+        correctAnswer:'b'    
+    },
+    {
+        title:' Sở thú bị cháy, con gì chạy ra đầu tiên?',
+        answer:{
+            a:'Con hổ',
+            b:'Con voi',
+            c:'Con khỉ',
+            d:'Con người'
+        },
+        correctAnswer:'d'    
+    }
+
+
+
+]
---
+++ src/quizz.js
@@ -0,0 +1,181 @@
+import {question} from './data.js';
+
+let currentQuestion = 0;
+let correctAnswer_num = 0;
+let nextQuestion = false;
+let finished = false;
+const questionNum = document.querySelector('.question-num');
+const questionContent = document.querySelector('.question-content');
+
+const answerA = document.getElementById('answerLabelA');
+const answerB = document.getElementById('answerLabelB');
+const answerC = document.getElementById('answerLabelC');
+const answerD = document.getElementById('answerLabelD');
+
+const answers_text = [answerA,answerB,answerC,answerD];
+const answers_radio = document.querySelectorAll('.answer-input');
+
+const correctAns = document.querySelector('.answer--correct');
+const wrongAns   = document.querySelector(' .answer--wrong');
+
+const notification = document.querySelector('.notification');
+const btnSubmit = document.querySelector('.submit-button');
+const resultText = document.querySelector('.result-text');
+const resultElement = document.querySelector('.result');
+
+
+const countdownTimer = document.querySelector('.countdown');
+function loadQuestion({title,answer,correctAnswer}){
+    questionNum.textContent = `Question ${currentQuestion+1}`;
+    questionContent.textContent = title;
+    answerA.textContent = answer.a;
+    answerB.textContent = answer.b;
+    answerC.textContent = answer.c;
+    answerD.textContent = answer.d;
+  
+}
+function resetResultAnswer(){
+    answers_radio.forEach((e)=>{e.checked =false});
+    answers_text.forEach((e)=>{e.classList.remove('answer--correct','answer--wrong')});
+
+}
+function getCurrentSelectedAnswer(){
+    let selected=null;
+    answers_radio.forEach((element)=>{
+        if(element.checked===true){
+            // console.log('Selected Element is',element.id);
+            selected = element.id;
+        }
+    });
+    return selected;
+}
+function showNotification(){
+    notification.classList.remove('notification--hidden');
+    notification.classList.add('notification--show');
+}
+function hiddenNotification(){
+    notification.classList.remove('notification--show');
+    notification.classList.add('notification--hidden');
+}
+function disabledSelectedAnswer(disabled){
+    answers_radio.forEach((item)=>{item.disabled=disabled});
+}
+function checkAnswer(){
+    const answer = getCurrentSelectedAnswer();
+    if(answer ==null){
+        // console.log('chua chon dap ap nao',answer);
+        // set notification
+        setNotification('Bạn chưa chọn đáp án nào nhá!');
+        return false;
+    }
+    disabledSelectedAnswer(true);
+    
+    if(answer == question[currentQuestion].correctAnswer){
+        correctAnswer_num++;
+        document.getElementById(answer).labels[0].classList.add('answer--correct');
+        // set notification for success answer
+        // console.log(answer);
+        console.log('Bạn đã trả lời chính xác rồi nha !');
+        setNotification('Bạn đã trả lời chính xác rồi nha !');
+    }
+    else{
+        document.getElementById(answer).labels[0].classList.add('answer--wrong');
+        document.getElementById(question[currentQuestion].correctAnswer).labels[0].classList.add('answer--correct');
+        // set notification for failure answer
+        // console.log('Bạn đã trả lời sai rồi hỉ!');
+        setNotification('Bạn đã trả lời sai rồi hỉ!');
+    }
+    if(countdownTimer.innerText =="Finished"){
+        correctAnswer_num--;   
+    }  
+
+    
+    return true;
+    
+}
+
+let idTimeOut;
+function changeButtonText(text){
+    btnSubmit.textContent = text;
+}
+function setNotification(message){
+    if(idTimeOut){
+        clearTimeout();
+    }
+    notification.textContent = message;
+    showNotification();
+    setTimeout(()=>{
+        hiddenNotification();
+
+    },1500);
+
+}
+let downloadTimer=null;
+function countdownGenerate(){
+let timeleft = 10;
+ downloadTimer = setInterval(()=>{
+    if(timeleft<=0){
+        clearInterval(downloadTimer);
+        countdownTimer.innerHTML = `Finished`;
+    }
+    else{
+        countdownTimer.innerHTML = `${timeleft} seconds remaining`;
+    }
+    timeleft--;
+},1000);
+
+}
+
+function handleClickSubmitButton(){
+    if(finished){
+        showFinalResult();
+    }
+
+    if(!nextQuestion){
+        if(checkAnswer()){
+            if(currentQuestion === question.length -1){
+                finished =true;
+                nextQuestion = false;
+                changeButtonText('Go to result🏆');
+            }
+            else{
+            currentQuestion++;
+            nextQuestion =true;
+            changeButtonText('Next question⏭️');
+            // console.log(currentQuestion);
+            clearInterval(downloadTimer);
+            // if(countdownTimer.innerText =="Finished"){
+            //     correctAnswer_num--;   
+            // }    
+            }
+        }
+    }
+    else{
+        nextQuestion=false;
+        resetResultAnswer();
+        changeButtonText('Submit');
+        disabledSelectedAnswer(false);
+        loadQuestion(question[currentQuestion]);
+        countdownGenerate();
+
+    }
+
+}
+function showFinalResult(){
+    resultText.textContent = `${correctAnswer_num}/${question.length}`;
+    resultElement.classList.add('result--show');
+
+}
+
+
+loadQuestion(question[0]);
+countdownGenerate();
+
+btnSubmit.addEventListener('click',handleClickSubmitButton);
+
+
+
+
+
+
+
